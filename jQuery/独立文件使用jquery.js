6117c332741fe3059e13231a6917dca0945dad66
modangtian <37525132+modangtian@users.noreply.github.com>
2018-05-07 20:45:07 +0800

    前端

---
+++ jQuery/独立文件使用jquery.js
@@ -0,0 +1,10 @@
+/*$(document).ready(function(){
+			$('button').click(function() {
+				$('p').hide();
+			});
+		});*/
+		$(function(){
+			$('button').click(function(){
+				$('#test').hide();
+			});
+		});
